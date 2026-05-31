d15f99a4ebd646522bce927124be0049fb7babf0
ahamSel <77988808+ahamSel@users.noreply.github.com>
2026-05-31 19:22:23 +0100

    Polish portfolio page styling

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,13 +1,15 @@
 const nav = document.querySelector("nav");
 
-let lastScrollY = window.scrollY;
-window.addEventListener("scroll", () => {
-  if (window.scrollY > lastScrollY) {
-    nav.classList.add("hide-nav");
-    document.body.classList.add("nav-hidden");
-  } else {
-    nav.classList.remove("hide-nav");
-    document.body.classList.remove("nav-hidden");
-  }
-  lastScrollY = window.scrollY;
-});
+if (nav) {
+  let lastScrollY = window.scrollY;
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+      nav.classList.add("hide-nav");
+      document.body.classList.add("nav-hidden");
+    } else {
+      nav.classList.remove("hide-nav");
+      document.body.classList.remove("nav-hidden");
+    }
+    lastScrollY = window.scrollY;
+  });
+}
